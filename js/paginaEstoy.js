d609b34e7337e96ef3d77520934fabd1572c336c
Tsammet <carlos61658011@gmail.com>
2024-03-02 21:59:32 -0500

    agregando nav Total

--- js/paginaEstoy.js
+++ js/paginaEstoy.js
@@ -41,6 +41,61 @@ const estudiantesPage = () => {
     paginaTarifas.style.display = "none";
 }
 
+const periodosPage = () => {
+    paginaAsignaturas.style.display = "none";
+    paginaDepartamentos.style.display = "none";
+    paginaEstudiantes.style.display = "none";
+    paginaPeriodos.style.display = "block";
+    paginaProfesores.style.display = "none";
+    paginaProgramas.style.display = "none";
+    paginaSalones.style.display = "none";
+    paginaTarifas.style.display = "none";
+}
+
+const profesoresPage = () => {
+    paginaAsignaturas.style.display = "none";
+    paginaDepartamentos.style.display = "none";
+    paginaEstudiantes.style.display = "none";
+    paginaPeriodos.style.display = "none";
+    paginaProfesores.style.display = "block";
+    paginaProgramas.style.display = "none";
+    paginaSalones.style.display = "none";
+    paginaTarifas.style.display = "none";
+}
+
+const programasPage = () => {
+    paginaAsignaturas.style.display = "none";
+    paginaDepartamentos.style.display = "none";
+    paginaEstudiantes.style.display = "none";
+    paginaPeriodos.style.display = "none";
+    paginaProfesores.style.display = "none";
+    paginaProgramas.style.display = "block";
+    paginaSalones.style.display = "none";
+    paginaTarifas.style.display = "none";
+}
+
+const salonesPage = () => {
+    paginaAsignaturas.style.display = "none";
+    paginaDepartamentos.style.display = "none";
+    paginaEstudiantes.style.display = "none";
+    paginaPeriodos.style.display = "none";
+    paginaProfesores.style.display = "none";
+    paginaProgramas.style.display = "none";
+    paginaSalones.style.display = "block";
+    paginaTarifas.style.display = "none";
+}
+
+const tarifasPage = () => {
+    paginaAsignaturas.style.display = "none";
+    paginaDepartamentos.style.display = "none";
+    paginaEstudiantes.style.display = "none";
+    paginaPeriodos.style.display = "none";
+    paginaProfesores.style.display = "none";
+    paginaProgramas.style.display = "none";
+    paginaSalones.style.display = "none";
+    paginaTarifas.style.display = "block";
+}
+
 const funcionPaginas = () => {
     const asignaturasNavLink = document.getElementById('asignaturasNav');
     asignaturasNavLink.addEventListener('click', asignaturasPage);
@@ -50,4 +105,19 @@ const funcionPaginas = () => {
 
     const estudiantesNavLink = document.getElementById('estudiantesNav');
     estudiantesNavLink.addEventListener('click', estudiantesPage)
+
+    const periodosNavLink = document.getElementById('periodosNav');
+    periodosNavLink.addEventListener('click', periodosPage)
+
+    const profesoresNavLink = document.getElementById('profesoresNav');
+    profesoresNavLink.addEventListener('click', profesoresPage)
+
+    const programasNavlink = document.getElementById('programasNav');
+    programasNavlink.addEventListener('click', programasPage)
+
+    const tarifasNavLink = document.getElementById('tarifasNav');
+    tarifasNavLink.addEventListener('click', tarifasPage)
+
+    const salonesNavLink = document.getElementById('salonesNav');
+    salonesNavLink.addEventListener('click', salonesPage)
 }
